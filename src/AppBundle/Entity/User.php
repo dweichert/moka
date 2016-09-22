1673d4740523c79dc43008b777edc9b41961e1c5
David Weichert <info@davidweichert.de>
2016-09-22 08:26:24 +0200

    Adds basic setup for FOS/user-bundle.

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of moka.
+ *
+ * (c) David Weichert <info@davidweichert.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
