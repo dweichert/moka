786bcc8d20af355a769e327578692b929fbdb110
David Weichert <info@davidweichert.de>
2016-09-24 10:21:14 +0200

    Extends user entity.

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -10,6 +10,7 @@
 
 namespace AppBundle\Entity;
 
+use DateTime;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -20,14 +21,207 @@ use Doctrine\ORM\Mapping as ORM;
 class User extends BaseUser
 {
     /**
+     * @var int
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
+    /**
+     * @var string
+     * @ORM\Column(name="firstname", type="string", length=255)
+     */
+    protected $firstName;
+
+    /**
+     * @var string
+     * @ORM\Column(name="lastname", type="string", length=255)
+     */
+    protected $lastName;
+
+    /**
+     * @var string
+     * @ORM\Column(name="street1", type="string", length=255)
+     */
+    protected $streetAddress;
+
+    /**
+     * @var string
+     * @ORM\Column(name="street2", type="string", length=255)
+     */
+    protected $streetAddress2;
+
+    /**
+     * @var string
+     * @ORM\Column(name="postalcode", type="string", length=25)
+     */
+    protected $postalCode;
+
+    /**
+     * @var string
+     * @ORM\Column(name="city", type="string", length=255)
+     */
+    protected $city;
+
+    /**
+     * @var string
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    protected $country;
+
+    /**
+     * @var DateTime|null
+     * @ORM\Column(name="birthday", type="date")
+     */
+    protected $birthday;
+
     public function __construct()
     {
         parent::__construct();
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreetAddress()
+    {
+        return $this->streetAddress;
+    }
+
+    /**
+     * @param string $streetAddress
+     * @return User
+     */
+    public function setStreetAddress($streetAddress)
+    {
+        $this->streetAddress = $streetAddress;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreetAddress2()
+    {
+        return $this->streetAddress2;
+    }
+
+    /**
+     * @param string $streetAddress2
+     * @return User
+     */
+    public function setStreetAddress2($streetAddress2)
+    {
+        $this->streetAddress2 = $streetAddress2;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostalCode()
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * @param string $postalCode
+     * @return User
+     */
+    public function setPostalCode($postalCode)
+    {
+        $this->postalCode = $postalCode;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     * @return User
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param string $country
+     * @return User
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+        return $this;
+    }
+
+    /**
+     * @return DateTime|null
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * @param DateTime|null $birthday
+     * @return User
+     */
+    public function setBirthday($birthday)
+    {
+        $this->birthday = $birthday;
+        return $this;
     }
 }
