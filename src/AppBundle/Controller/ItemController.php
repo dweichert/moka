308c8b84f070cecff567b6f30b11a1fcec4b9f65
David Weichert <info@davidweichert.de>
2016-10-09 16:16:32 +0200

    Adds implementation for delete item action (#2).

--- src/AppBundle/Controller/ItemController.php
+++ src/AppBundle/Controller/ItemController.php
@@ -11,10 +11,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Item;
+use Assert\Assertion;
+use Assert\AssertionFailedException;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Csrf\CsrfToken;
 
 class ItemController extends Controller
 {
@@ -88,6 +91,30 @@ class ItemController extends Controller
      */
     public function deleteAction(Request $request)
     {
+        $token = $request->request->get('_csrf_token');
+        $csrfToken = new CsrfToken('administrate-items', $token);
+        if (!$this->isCsrfTokenValid('administrate-items', $csrfToken)) {
+            $this->addFlash('error', 'Invalid request, please try again.');
+            return $this->redirectToRoute('item_list');
+        }
 
+        try {
+            $id = $request->get('current-data-id');
+            Assertion::integerish($id);
+        } catch (AssertionFailedException $e) {
+            $this->addFlash('error', 'Invalid item, please try again.');
+            return $this->redirectToRoute('item_list');
+        }
+
+        $item = $this->getDoctrine()->getRepository('AppBundle:Item')->find($id);
+        if (is_null($item)) {
+            $this->addFlash('error', 'Could not find item, please try again.');
+            return $this->redirectToRoute('item_list');
+        }
+
+        $this->getDoctrine()->getManager()->remove($item);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->redirectToRoute('item_list');
     }
 }
