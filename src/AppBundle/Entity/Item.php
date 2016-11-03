be30e5c6cc593572e8d3c2a5aa17bc7d6cd24ea2
David Weichert <info@davidweichert.de>
2016-11-03 20:47:51 +0100

    Adds new properties class and quantity.

--- src/AppBundle/Entity/Item.php
+++ src/AppBundle/Entity/Item.php
@@ -22,6 +22,8 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Item
 {
+    const UNLIMITED_QUANTITY = -1;
+
     /**
      * @var int
      *
@@ -76,6 +78,28 @@ class Item
      * @ORM\Column(name="weight", type="integer")
      */
     private $weight;
+
+    /**
+     * Class ID.
+     *
+     * Items of the same class come into existence as copies of the original
+     * item of this class, this is the case if the quantity required is
+     * greater than one.
+     *
+     * @var string
+     *
+     * @ORM\Column(name="class", type="string", length=20)
+     */
+    private $class;
+
+    /**
+     * Number of items of this class required.
+     *
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer")
+     */
+    private $quantity;
 
     /**
      * Get id
@@ -221,12 +245,89 @@ class Item
      * Set weight.
      *
      * @param int $weight
+     *
+     * @return Item
      */
     public function setWeight($weight)
     {
         $this->weight = $weight;
+
+        return $this;
     }
 
+    /**
+     * Get class.
+     *
+     * @return string
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+
+    /**
+     * Set class.
+     *
+     * Sets class name to unique id if no name is given. Maximum 20 characters
+     * allowed, longer class names will be truncated to 20 characters.
+     *
+     * @param string $name OPTIONAL
+     *
+     * @return Item
+     */
+    public function setClass($name = null)
+    {
+        if ($name) {
+            $this->class = substr($name, 0, 19);
+        } else {
+            $this->class = uniqid();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get quantity.
+     *
+     * Returns either a positive integer specifying the number of times this
+     * item can be pledged or 'unlimited' to indicate that this item can be
+     * pledged any number of times.
+     *
+     * @return int|string
+     */
+    public function getQuantity()
+    {
+        if ($this->quantity == self::UNLIMITED_QUANTITY) {
+            return 'unlimited';
+        }
+        if ($this->quantity == 0) {
+            return 1;
+        }
+
+        return $this->quantity;
+    }
+
+    /**
+     * Set quantity.
+     *
+     * Allows for either a positive integer specifying the number of times this
+     * item can be pledged or 'unlimited' to indicate that this item can be
+     * pledged any number of times.
+     *
+     * @param int|string $quantity OPTIONAL defaults to 1
+     *
+     * @return Item
+     */
+    public function setQuantity($quantity = 1)
+    {
+        if ($quantity == 'unlimited') {
+            $this->quantity = self::UNLIMITED_QUANTITY;
+        }
+
+        $this->quantity = $quantity;
+
+        return $this;
+    }
 
 }
 
