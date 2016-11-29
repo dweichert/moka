acc67d5f27d67027ea6922f9c05b0f70834a91b8
David Weichert <info@davidweichert.de>
2016-11-29 21:05:27 +0100

    Upon cancelling pledge remove excess unpledged items of same class.

--- src/AppBundle/Repository/ItemRepository.php
+++ src/AppBundle/Repository/ItemRepository.php
@@ -69,6 +69,9 @@ class ItemRepository extends EntityRepository
             ->getResult();
     }
 
+    /**
+     * @return Item[]
+     */
     public function findAllItemsThatCanSpawn()
     {
         $qb = $this->createQueryBuilder('i');
